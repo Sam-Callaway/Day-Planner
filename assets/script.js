d53f73dc3b5c0533938df05440bddb9256e6eedb
Sam Callaway <calls002@gmail.com>
2023-02-20 17:52:53 +0000

    Added highlighting function for hour rows based on current time

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,27 @@
+var scheduleGrid = $("#scheduleGrid")
+// Set the current day text at the top of the page
 $("#currentDay").text(moment().format("Do MMMM YYYY"))
 
 
+// This function highlights the hour rows depending on if they are in the past, present or future
+function hourHighlight(){
+    for (i=0;i<15;i++){
+        var hourRow = scheduleGrid.children().eq(i)
+        var hourRowHour = moment(hourRow.children().first().text(),'hh:mm A').format('kk')
+        var nowHour = moment().format('kk')
+        if(hourRowHour < nowHour){
+            hourRow.children().eq(1).addClass('past')
+        }
+        else
+        if (hourRowHour === nowHour){
+            hourRow.children().eq(1).addClass('present')
+        }
+        else
+        {
+            hourRow.children().eq(1).addClass('future')
+        }
+    
+    }
+}
+
+hourHighlight();
